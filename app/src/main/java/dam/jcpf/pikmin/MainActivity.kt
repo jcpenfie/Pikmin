64fab17854bcd10b4c979bfe2b7988ee0e9a4378
jcpenfie <juancarlospfierro@gmail.com>
2025-11-11 22:11:33 +0100

    Implementación de la toolbar y de la flechita para volver atrás desde el detalle del pikmin
    Signed-off-by: jcpenfie <juancarlospfierro@gmail.com>

--- app/src/main/java/dam/jcpf/pikmin/MainActivity.kt
+++ app/src/main/java/dam/jcpf/pikmin/MainActivity.kt
@@ -14,8 +14,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configurar toolbar como ActionBar
+        setSupportActionBar(binding.toolbar)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        // Cambio el título según el destino
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id){
+                R.id.listFragment -> {
+                    supportActionBar?.title = "Pikmin"
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                R.id.detailsFragment -> {
+                    supportActionBar?.title = "Detalle Pikmin"
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                }
+            }
+        }
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 }
